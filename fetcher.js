0653e8340bf00b4b1c56cfda26f20b7da67ffa91
Wilfred <wilfredemonts@gmail.com>
2022-01-06 23:13:03 -0500

    created app to download page and save it to specified local path

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const fs = require('fs');
+
+let content = "";
+
+request(process.argv[2], (error, response, body) => {
+  
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log(body.length);// Print the HTML for the Google homepage.
+  content = body;
+
+    fs.writeFile(process.argv[3], content, err => {
+      if (err) {
+       console.error(err)
+        return
+    }
+    //file written successfully
+  })
+});
+
+
+
+
+
+
